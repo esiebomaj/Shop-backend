47cf20da90af8503ebdf9e01f20a5feb373fdc79
Emma <macanthonyemmanuel10@gmail.com>
2021-04-18 19:27:31 -0700

    added new store app

--- shopbackend/settings.py
+++ shopbackend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # store apps
+    'store',
+    
+    # 3rd party apps
 ]
 
 MIDDLEWARE = [
